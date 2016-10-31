edf6089c4e979583608ab554a9ee91c9c47dcab6
Mark Ng'ang'a <maqnganga@gmail.com>
2016-10-31 03:48:27 +0300

    Added requirements

--- dictionary.py
+++ dictionary.py
@@ -8,6 +8,10 @@ def main(argv):
         Script to get the definition of a word from wordnik.com
         Sign up and get a FREE API key from http://developer.wordnik.com/ before use
 
+        Requirements:
+            The official Python client library for the Wordnik API - Install using pip install wordnik
+            Wordnik API key - Sign up for one for free at http://developer.wordnik.com/
+
         Options:
             -w or --word - The word you wish to search for
             -l or --limit - The number of defitions you would like to get back
